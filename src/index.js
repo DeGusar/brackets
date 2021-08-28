63875d35e9a799eea7157c584453ad96e8af3a1e
Denis Gusar <degusar.wot@gmail.com>
2021-08-28 17:36:54 +0300

    feat: finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsObject = new Object();
+  let arrOpenBrackets = [];
+  for (let i of bracketsConfig) {
+    bracketsObject[i[1]] = i[0];
+  };
+  for (let i of bracketsConfig) {
+    arrOpenBrackets.push(i[0]);
+  };
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+    if (arrOpenBrackets.includes(currentSymbol)) {
+      if (bracketsObject[currentSymbol] === currentSymbol && topElement === currentSymbol) {
+        stack.pop();
+      } else {
+         stack.push(currentSymbol);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+     
+
+      if (bracketsObject[currentSymbol] === topElement) {
+       stack.pop();
+      } else {
+        return false;
+      }
+    }
+   
+  }
+   return stack.length === 0;
+ 
 }
